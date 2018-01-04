a1e4ed2b49224ee36fd145915ef13164305ef26d
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-01-04 09:00:24 -0800

    Add DATABASE_URL env var

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 'use strict';
 
+const DATABASE_URL = process.env.DATABASE_URL;
+
+exports.DATABASE = {
+  client: 'pg',
+  connection: DATABASE_URL,
+  pool: {min: 0, max: 3},
+  debug: false
+};
+
 exports.PORT = process.env.PORT || 8080; 
